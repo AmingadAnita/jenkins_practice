7c6ea40febe8f797d15c5b7d8f98c2c180c24b2c
AmingadAnita <33828743+AmingadAnita@users.noreply.github.com>
2020-04-11 19:53:58 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 System.out.println("Hello World");
 System.out.println("added from github");
 System.out.println("added for poll scm");
+System.out.println("added for poll scm 2");
 }
 }
